16c0b1516cda290642ab87cbe0e19e00964f8758
eramitgupta <info.eramitgupta@gmail.com>
2025-12-01 17:07:47 +0530

    feat: final working vue/react multi-entry package setup

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,29 +1,29 @@
-const resolve = require('@rollup/plugin-node-resolve')
-const commonjs = require('@rollup/plugin-commonjs')
-const typescript = require('rollup-plugin-typescript2')
-const pkg = require('./package.json')
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('rollup-plugin-typescript2');
+const pkg = require('./package.json');
 
-module.exports = {
+const externalDeps = [...Object.keys(pkg.peerDependencies || {})];
+
+module.exports = [
+  {
     input: 'src/index.ts',
-    external: [...Object.keys(pkg.peerDependencies || {})],
-    output: [
-        {
-            file: pkg.module,
-            format: 'es',
-            sourcemap: true
-        },
-        {
-            file: pkg.main,
-            format: 'cjs',
-            sourcemap: true,
-            exports: 'named'
-        }
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript({
-            tsconfigOverride: { compilerOptions: { declaration: true } }
-        })
-    ]
-}
+    external: externalDeps,
+    output: [{ file: 'dist/index.js', format: 'esm', sourcemap: true }],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+
+  {
+    input: 'src/vue/index.ts',
+    external: externalDeps,
+    output: [{ file: 'dist/vue/index.js', format: 'esm', sourcemap: true }],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+
+  {
+    input: 'src/react/index.ts',
+    external: externalDeps,
+    output: [{ file: 'dist/react/index.js', format: 'esm', sourcemap: true }],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export * as VueLang from './vue/useLang'
-export * as ReactLang from './react/useLang'
-
-export * from './types/lang'
+export * from './types/lang';
