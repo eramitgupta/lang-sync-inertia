aafb2dfaf55abbb963d51775b87c7307272ee0e7
eramitgupta <info.eramitgupta@gmail.com>
2025-12-01 17:34:32 +0530

    chore: fix

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -3,27 +3,31 @@ const commonjs = require('@rollup/plugin-commonjs');
 const typescript = require('rollup-plugin-typescript2');
 const pkg = require('./package.json');
 
-const external = [
-    ...Object.keys(pkg.peerDependencies || {})
-];
+const external = Object.keys(pkg.peerDependencies || {});
 
 module.exports = [
-    {
-        input: 'src/index.ts',
-        external,
-        output: [{ file: 'dist/index.js', format: 'esm', sourcemap: true }],
-        plugins: [resolve(), commonjs(), typescript()],
-    },
-    {
-        input: 'src/vue/index.ts',
-        external,
-        output: [{ file: 'dist/vue/index.js', format: 'esm', sourcemap: true }],
-        plugins: [resolve(), commonjs(), typescript()],
-    },
-    {
-        input: 'src/react/index.ts',
-        external,
-        output: [{ file: 'dist/react/index.js', format: 'esm', sourcemap: true }],
-        plugins: [resolve(), commonjs(), typescript()],
-    },
+  {
+    input: 'src/index.ts',
+    external,
+    output: [
+      { file: 'dist/index.js', format: 'esm', sourcemap: true }
+    ],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+  {
+    input: 'src/vue/index.ts',
+    external,
+    output: [
+      { file: 'dist/vue/index.js', format: 'esm', sourcemap: true }
+    ],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
+  {
+    input: 'src/react/index.ts',
+    external,
+    output: [
+      { file: 'dist/react/index.js', format: 'esm', sourcemap: true }
+    ],
+    plugins: [resolve(), commonjs(), typescript()],
+  },
 ];
