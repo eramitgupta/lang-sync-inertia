4ba5578a273b45b8a331a93efa3d795e38250fa8
eramitgupta <info.eramitgupta@gmail.com>
2026-03-30 17:55:44 +0530

    fix(rollup): prevent react types from being emitted under vue build

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -4,6 +4,12 @@ const typescript = require('rollup-plugin-typescript2');
 const pkg = require('./package.json');
 
 const external = Object.keys(pkg.peerDependencies || {});
+const createTypescriptPlugin = (include) =>
+  typescript({
+    tsconfigOverride: {
+      include,
+    },
+  });
 
 module.exports = [
   {
@@ -12,7 +18,7 @@ module.exports = [
     output: [
       { file: 'dist/index.js', format: 'esm', sourcemap: true }
     ],
-    plugins: [resolve(), commonjs(), typescript()],
+    plugins: [resolve(), commonjs(), createTypescriptPlugin(['src/index.ts', 'src/types/**/*'])],
   },
   {
     input: 'src/vue/index.ts',
@@ -20,7 +26,7 @@ module.exports = [
     output: [
       { file: 'dist/vue/index.js', format: 'esm', sourcemap: true }
     ],
-    plugins: [resolve(), commonjs(), typescript()],
+    plugins: [resolve(), commonjs(), createTypescriptPlugin(['src/vue/**/*', 'src/types/**/*'])],
   },
   {
     input: 'src/react/index.ts',
@@ -28,6 +34,6 @@ module.exports = [
     output: [
       { file: 'dist/react/index.js', format: 'esm', sourcemap: true }
     ],
-    plugins: [resolve(), commonjs(), typescript()],
+    plugins: [resolve(), commonjs(), createTypescriptPlugin(['src/react/**/*', 'src/types/**/*'])],
   },
 ];
